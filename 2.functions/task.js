c3f81e734f8145764bb6862a96c47a54fe1d07a5
Sergey Kievskiy <knhel7@mail.ru>
2022-08-04 18:37:42 +0300

    Edit task.js. First variant for three tasks

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,58 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
-
   // Ваш код
-
+  min = Infinity;
+  max = -Infinity;
+  sum = 0;
+  for (let i in arr) {
+    let a = parseFloat(arr[i]);
+    if (isNaN(a)) {
+      a = 0;
+    }
+    if (a > max) {
+      max = a;
+    }
+    if (a < min) {
+      min = a;
+    }
+    sum += a;
+  }
+  avg = +((sum / arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
   let sum;
-
   // Ваш код
-
+  sum = 0;
+  for (let i in arr) {
+    let a = parseFloat(arr[i]);
+    if (isNaN(a)) {
+      a = 0;
+    }
+    sum += a;
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
   let max;
-
   // Ваш кода
-  // for ...
-  
+  max = -Infinity;
+  for (let i in arrOfArr) {
+    let sum = func(arrOfArr[i]);
+    if (sum > max) {
+      max = sum;
+    }
+  }
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
   // Ваш код
+  let params = getArrayParams(arr);
+  return Math.abs(params.max - params.min);
 }
