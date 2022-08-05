8e5d8c2f29605702781cd730c8c9fa6b11d1ffe5
Sergey Kievskiy <knhel7@mail.ru>
2022-08-05 23:31:32 +0300

    Edit task.js. First ed. for two tasks

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,13 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  return  arr1.every((i) => arr1[i] === arr2[i]);
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  return arr
+      .filter((a) => a > 0)
+      .filter((a) => a % 3 === 0)
+      .map((a) => a * 10);
 }
