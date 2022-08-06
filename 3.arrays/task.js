5a2be4a03575bba84ee4ec63db2dcb0645b6e6ba
Sergey Kievskiy <knhel7@mail.ru>
2022-08-06 11:02:14 +0300

    Edit task.js, the third ed. for task1

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,6 +1,17 @@
 function compareArrays(arr1, arr2) {
   return  arr1.length === arr2.length
-      &&  arr1.every((i) => arr1[i-1] === arr2[i-1]);
+      &&  arr1.every((a,i) => a === arr2[i]);
+
+  // function isEqual(a, i, arr, arr_2 = arr2, a2 = arr_2[i]) {
+  //   alert(`${arr}\n${arr_2}\ni=${i}:   ` +
+  //       `arr1[${i}]=${a}   ===? ` +
+  //       `(${a === a2})   ` +
+  //       `arr2[${i}]=${a2}`);
+  //   return a === a2;
+  // }
+  //
+  // return  arr1.length === arr2.length
+  //     &&  arr1.every(isEqual);
 }
 
 function advancedFilter(arr) {
